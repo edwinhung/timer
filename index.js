a4871b4508a8671156fb36fbe5f7a56e5d60eb96
Edwin Hung <32249535+edwinhung@users.noreply.github.com>
2024-06-21 20:37:26 -0700

    add Timer class

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Hi!!");
+class Timer {
+  constructor(durationInput, startBtn, pauseBtn) {
+    this.durationInput = durationInput;
+    this.startBtn = startBtn;
+    this.pauseBtn = pauseBtn;
+
+    this.startBtn.addEventListener("click", this.start);
+  }
+
+  start() {
+    console.log("Start Timer!");
+  }
+}
