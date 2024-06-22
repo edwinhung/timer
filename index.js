7b30fef64164c80d12594d230bb9dc4146364afe
Edwin Hung <32249535+edwinhung@users.noreply.github.com>
2024-06-22 15:30:12 -0700

    add emit events

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 class Timer {
-  constructor(durationInput, startBtn, pauseBtn) {
+  constructor(durationInput, startBtn, pauseBtn, callbacks) {
     this.durationInput = durationInput;
     this.startBtn = startBtn;
     this.pauseBtn = pauseBtn;
+    if (callbacks) {
+      this.onStart = callbacks.onStart;
+      this.onTick = callbacks.onTick;
+      this.onComplete = callbacks.onComplete;
+    }
 
     this.startBtn.addEventListener("click", this.start);
     this.pauseBtn.addEventListener("click", this.pause);
   }
 
   start = () => {
+    if (this.onStart) {
+      this.onStart();
+    }
     this.tick();
     this.interval = setInterval(this.tick, 1000);
   };
@@ -16,7 +24,11 @@ class Timer {
   tick = () => {
     if (this.timeLeft <= 0) {
       this.pause();
+      if (this.onComplete) {
+        this.onComplete();
+      }
     } else {
+      this.onTick();
       this.timeLeft = this.timeLeft - 1;
     }
   };
@@ -37,4 +49,14 @@ class Timer {
 const durationInput = document.querySelector("#duration");
 const startBtn = document.querySelector("#start");
 const pauseBtn = document.querySelector("#pause");
-const timer = new Timer(durationInput, startBtn, pauseBtn);
+const timer = new Timer(durationInput, startBtn, pauseBtn, {
+  onStart() {
+    console.log("Started");
+  },
+  onTick() {
+    console.log("tick");
+  },
+  onComplete() {
+    console.log("Completed");
+  },
+});
