dd4a1e8a8ec61ece4098c0bc516201e21bffee8e
Edwin Hung <32249535+edwinhung@users.noreply.github.com>
2024-06-23 14:15:57 -0700

    set up circle animation

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const durationInput = document.querySelector("#duration");
 const startBtn = document.querySelector("#start");
 const pauseBtn = document.querySelector("#pause");
+
+const circle = document.querySelector("circle");
+const perimeter = circle.getAttribute("r") * 2 * Math.PI;
+circle.setAttribute("stroke-dasharray", perimeter);
+
+let currentOffset = 0;
 const timer = new Timer(durationInput, startBtn, pauseBtn, {
   onStart() {
     console.log("Started");
   },
   onTick() {
-    console.log("tick");
+    circle.setAttribute("stroke-dashoffset", currentOffset);
+    currentOffset -= 50;
   },
   onComplete() {
     console.log("Completed");
